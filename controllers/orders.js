89b89ded23cb1bf729d6eb9fcd18ec2340159483
ad.hanif <ad.hanif@outlook.com>
2023-06-20 22:47:11 +0200

    POST(create a new order implemented)

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,13 +1,46 @@
-const Pool = require("../db");
+const pool = require("../db");
 
+//get all orders
 const getOrders = async (req, res) => {
   try {
-    const { rows } = await Pool.query("select * from orders;");
-    res.send(rows);
+    const { rows } = await pool.query("select * from orders;");
+    res.json(rows);
   } catch (error) {
     console.log(error.message);
     res.status(500).send("something went wrong");
   }
 };
 
-module.exports = { getOrders };
+// get a order by id
+
+const orderById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { rows } = await pool.query("SELECT * FROM orders where id=$1;", [
+      id,
+    ]);
+    res.json(rows);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("something went wrong");
+  }
+};
+
+//POST / -> To create a new user
+
+const editOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { price, date, user_id } = req.body;
+    const { rows } = await pool.query(
+      "insert into orders (price, date, user_id) values($1, $2, $3) RETURNING *;",
+      [price, date, user_id]
+    );
+    res.json(rows);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("something went wrong");
+  }
+};
+
+module.exports = { getOrders, orderById, editOrder };
--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const ordersRouter = express.Router();
-const { getOrders } = require("../controllers/orders");
+const { getOrders, orderById, editOrder } = require("../controllers/orders");
+//get all orders
 ordersRouter.get("/", getOrders);
+// get a order by id
+ordersRouter.get("/:id", orderById);
+//POST / -> To create a new user
+ordersRouter.post("/", editOrder);
+
 module.exports = ordersRouter;
