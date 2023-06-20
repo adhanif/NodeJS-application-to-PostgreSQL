5e5cb03a1709ed2fd572f776014eecf3f6a3bcea
ad.hanif <ad.hanif@outlook.com>
2023-06-20 23:20:32 +0200

    PUT and DELETE implemented

--- controllers/orders.js
+++ controllers/orders.js
@@ -28,9 +28,8 @@ const orderById = async (req, res) => {
 
 //POST / -> To create a new user
 
-const editOrder = async (req, res) => {
+const createOrder = async (req, res) => {
   try {
-    const { id } = req.params;
     const { price, date, user_id } = req.body;
     const { rows } = await pool.query(
       "insert into orders (price, date, user_id) values($1, $2, $3) RETURNING *;",
@@ -43,4 +42,40 @@ const editOrder = async (req, res) => {
   }
 };
 
-module.exports = { getOrders, orderById, editOrder };
+// PUT /:id  :  To edit one order (with the id)
+
+const editOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { price, date, user_id } = req.body;
+    const { rows } = await pool.query(
+      "UPDATE orders SET price=$1, date=$2, user_id=$3 WHERE id=$4 RETURNING *;",
+      [price, date, user_id, id]
+    );
+    res.json(rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Something went wrong");
+  }
+};
+
+//DELETE  /:id : To delete one order (with the id)
+const deleteOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const { first_name, last_name, age } = req.body;
+    const { rows } = await pool.query("DELETE FROM orders WHERE id=$1;", [id]);
+    res.json(rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("something went wrong");
+  }
+};
+
+module.exports = {
+  getOrders,
+  orderById,
+  editOrder,
+  createOrder,
+  deleteOrder,
+};
--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const ordersRouter = express.Router();
-const { getOrders, orderById, editOrder } = require("../controllers/orders");
+const {
+  getOrders,
+  orderById,
+  createOrder,
+  editOrder,
+  deleteOrder,
+} = require("../controllers/orders");
 //get all orders
 ordersRouter.get("/", getOrders);
 // get a order by id
 ordersRouter.get("/:id", orderById);
-//POST / -> To create a new user
-ordersRouter.post("/", editOrder);
+//POST/ -> To create a new order
+ordersRouter.post("/", createOrder);
+// PUT/:id => To edit one order (with the id)
+ordersRouter.put("/:id", editOrder);
+
+// DELETE/:id :To delete one order (with the id)
+ordersRouter.delete("/:id", deleteOrder);
 
 module.exports = ordersRouter;
